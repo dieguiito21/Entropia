06c1f6c9c94071b2dc21da90c1e3c521187a08c6
Diego Castro <38577317+dieguiito21@users.noreply.github.com>
2025-05-13 22:27:32 -0600

    Add files via upload

---
+++ imagen.js
@@ -0,0 +1,86 @@
+document.getElementById("subirImagen").addEventListener('change', function (e) {
+    const archivo = e.target.files[0];
+    if (!archivo) return;
+
+    const img = new Image();
+    const lector = new FileReader();
+
+    lector.onload = function (event) {
+        img.src = event.target.result;
+        document.getElementById("vistaPrevia").src = event.target.result;
+    };
+
+    img.onload = function () {
+        const canvas = document.getElementById('canvas');
+        const ctx = canvas.getContext('2d');
+
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const data = imageData.data;
+
+        const histograma = new Array(256).fill(0);          // Convertir a escala de grises
+        const totalPixels = canvas.width * canvas.height;   
+
+        for (let i = 0; i < data.length; i += 4) {
+            const r = data[i];
+            const g = data[i + 1];
+            const b = data[i + 2];
+            const a = data[i + 3];
+
+            if (a === 0) continue; // Omitir negros innecesarios
+
+            const gray = Math.round(0.299 * r + 0.587 * g + 0.114 * b); // Luma
+            histograma[gray]++;
+        }
+
+        const pixeles = document.getElementById("pixeles"); 
+        pixeles.innerHTML = "";
+
+        for (let i = 0; i < 256; i++) {     // Impresión de la escala de grises
+            if (histograma[i] > 0) {
+                const p = document.createElement("p");
+                p.textContent = `Gris ${i}: ${histograma[i]} veces`;
+                pixeles.appendChild(p);
+            }
+        }
+
+        // Calcular las probabilidades de cada pixel
+        document.getElementById("probabilidades").innerHTML = "Probabilidades de cada caracter: ";
+        const probabilidades = document.getElementById("probabilidades");
+
+        let entropia = 0;
+        for (let i = 0; i < 256; i++) {
+            if (histograma[i] === 0) continue;
+            const p = histograma[i] / totalPixels;
+
+            const prob = document.createElement("p");
+            prob.textContent = `${i} -> ${histograma[i]}/${totalPixels}`;
+            probabilidades.appendChild(prob);
+
+            entropia -= p * Math.log2(p);
+            document.getElementById('formula').innerText = ` ${p}  * log2`;
+        }
+
+        const formula = document.getElementById("formula");
+        const imprimirEntropia = document.createElement("p");
+        formula.innerHTML = "";
+        document.getElementById("formula").innerHTML = "Cálculo de la Entropía: ";
+        imprimirEntropia.textContent = "H = - [ ";
+
+        for (let i = 0; i < 256; i++) { // Impresión de la formula
+            imprimirEntropia.textContent += `${histograma[i]}/${totalPixels} · log₂ (${histograma[i]}/${totalPixels}) + `;
+        }
+
+        imprimirEntropia.textContent = imprimirEntropia.textContent.slice(0,-2);
+        imprimirEntropia.textContent += ` ] = ${entropia.toFixed(4)}`;
+        formula.appendChild(imprimirEntropia);
+
+        document.getElementById("resultado").innerText = `Entropía: ${Math.ceil(entropia)} bits por píxel`;
+    };
+
+    lector.readAsDataURL(archivo);
+});
+
---
+++ texto.js
@@ -0,0 +1,80 @@
+function leerTexto(){   
+        
+        const textoX = document.getElementById("cajaDeTexto").value; // Guardamos el texto escrito en la caja
+        if(document.getElementById("cajaDeTexto").value == ""){
+            limpiar();
+            document.getElementById("textoIngresado").innerHTML = `No ingresaste ningún texto`;    
+            return;
+        } else document.getElementById("cajaDeTexto").value = "";
+        texto = textoX.trimEnd();
+        
+        document.getElementById("textoIngresado").innerHTML = "";
+        document.getElementById("textoIngresado").innerHTML = `Texto ingresado: ${texto}`;
+
+        const frecuencias = {};
+        const probs = {};
+        const caracteres = document.getElementById("caracteres");
+        const probabilidades = document.getElementById("probabilidades");
+        caracteres.innerHTML = ""; // Limpiar lo anterior escrito
+        probabilidades.innerHTML = "";
+
+        for(let i = 0; i < texto.length; i++){      // For para contar las veces que aparece cada caracter
+            const x = texto[i];
+            frecuencias[x] = (frecuencias[x] || 0) + 1; 
+        }
+
+        document.getElementById("caracteres").innerHTML = "Frecuencias de cada caracter: ";
+        for(const caracter in frecuencias){         // For para mostrar todos los caracteres y sus frecuencias
+            const p = document.createElement("p");
+            p.textContent = `${caracter} : ${frecuencias[caracter]}`;
+            caracteres.appendChild(p);
+        }
+
+        const formula = document.getElementById("formula");
+        const imprimirEntropia = document.createElement("p");
+        formula.innerHTML = "";
+        document.getElementById("formula").innerHTML = "Cálculo de la Entropía: ";
+        imprimirEntropia.textContent = "H = - [ ";
+
+        document.getElementById("probabilidades").innerHTML = "Probabilidades de cada caracter: ";
+        for(const caracter in frecuencias){
+            probs[caracter] = frecuencias[caracter]/texto.length;
+            const prob = document.createElement("p");
+            prob.textContent = `${caracter} -> ${frecuencias[caracter]}/${texto.length}`;
+
+            imprimirEntropia.textContent += `${frecuencias[caracter]}/${texto.length} · log₂ (${frecuencias[caracter]}/${texto.length}) + `;
+
+            probabilidades.appendChild(prob);   
+        }
+
+        imprimirEntropia.textContent = imprimirEntropia.textContent.slice(0,-2);
+        imprimirEntropia.textContent += " ]";
+        formula.appendChild(imprimirEntropia);
+
+        let entropia = 0;
+
+        for(const caracter in frecuencias){
+           entropia += (probs[caracter] * Math.log2(probs[caracter]));
+        }
+        entropia = entropia * (-1);
+        console.log(entropia);
+
+        const calculoH = document.getElementById("calculoH");
+        const resultado = document.createElement("p");
+        calculoH.innerHTML = "";
+        resultado.textContent = `H = ${entropia.toFixed(4)} = ${Math.ceil(entropia)} bits`;
+        calculoH.appendChild(resultado); 
+
+}
+
+function limpiar(){
+    document.getElementById("textoIngresado").innerHTML = "";
+    document.getElementById("cajaDeTexto").value = "";
+    document.getElementById("probabilidades").innerHTML = "";
+    document.getElementById("formula").innerHTML = "";
+    document.getElementById("caracteres").innerHTML = "";
+    document.getElementById("cajaDeTexto").innerHTML = "";
+    document.getElementById("calculoH").innerHTML = "";
+
+}
+   
